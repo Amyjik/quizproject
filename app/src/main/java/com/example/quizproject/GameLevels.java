fcbb86641e60ca4d33d6ca571004bcc158a09510
Amyjik <chelovekmen885@gmail.com>
2022-02-24 18:56:35 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quizproject/GameLevels.java
@@ -0,0 +1,61 @@
+package com.example.quizproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = (Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        textView1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    startActivity(intent);finish();
+                } catch(Exception e) {
+
+                }
+            }
+        }));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);finish();
+        } catch(Exception e) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quizproject/Level1.java
@@ -0,0 +1,26 @@
+package com.example.quizproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class Level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
+
+        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        img_right.setClipToOutline(true);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
