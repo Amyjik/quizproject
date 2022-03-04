d6822b29cdea8445765cd3a1a6d54cf8935de4fb
Amyjik <chelovekmen885@gmail.com>
2022-03-04 17:28:57 +0500

    qwqw

---
+++ app/src/main/java/com/example/quizproject/Array.java
@@ -0,0 +1,33 @@
+package com.example.quizproject;
+
+public class Array {
+
+    final int[] images1 = {
+            R.drawable.nulll,
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six,
+            R.drawable.seven,
+            R.drawable.eight,
+            R.drawable.nine,
+    };
+
+
+    final int[] texts1 = {
+            R.string.lvl1text0,
+            R.string.lvl1text1,
+            R.string.lvl1text2,
+            R.string.lvl1text3,
+            R.string.lvl1text4,
+            R.string.lvl1text5,
+            R.string.lvl1text6,
+            R.string.lvl1text7,
+            R.string.lvl1text8,
+            R.string.lvl1text9
+    };
+
+
+}
--- app/src/main/java/com/example/quizproject/Level1.java
+++ app/src/main/java/com/example/quizproject/Level1.java
@@ -7,16 +7,27 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Level1 extends AppCompatActivity {
 
     Dialog dialog;
+
+    public int numLeft;
+    public int numRight;
+    Array array = new Array();
+    Random random = new Random();
+    public int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,9 @@ public class Level1 extends AppCompatActivity {
 
         final ImageView img_right = (ImageView)findViewById(R.id.img_right);
         img_right.setClipToOutline(true);
+
+        final TextView text_left = findViewById(R.id.text_left);
+        final TextView text_right = findViewById(R.id.text_right);
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
@@ -80,6 +94,170 @@ public class Level1 extends AppCompatActivity {
             }
         });
 
+        final int[] progress = {
+                R.id.point1, R.id.point2, R.id.point3, R.id.point4, R.id.point5, R.id.point6, R.id.point7, R.id.point8, R.id.point9, R.id.point10,
+                R.id.point11, R.id.point12, R.id.point13, R.id.point14, R.id.point15, R.id.point16, R.id.point17, R.id.point18, R.id.point19, R.id.point20,
+        };
+
+        final Animation a = AnimationUtils.loadAnimation(Level1.this, R.anim.alpha);
+
+        numLeft = random.nextInt(10);
+        img_left.setImageResource(array.images1[numLeft]);
+        text_left.setText(array.texts1[numLeft]);
+
+        numRight = random.nextInt(10);
+
+
+        while(numLeft == numRight) {
+            numRight = random.nextInt(10);
+        }
+
+        img_right.setImageResource(array.images1[numRight]);
+        text_right.setText(array.texts1[numRight]);
+
+        //левая кнопка - начало
+        img_left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    img_right.setEnabled(false);
+                    if(numLeft > numRight) {
+                        img_left.setImageResource(R.drawable.img_true);
+                    } else{
+                        img_left.setImageResource(R.drawable.img_false);
+                    }
+                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if(numLeft > numRight) {
+                        if(count < 20) {
+                            count++;
+                        }
+
+                        for (int i = 0; i < 20; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for(int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+
+                    } else {
+                        if(count > 0) {
+                            if(count == 1) {
+                                count = 0;
+                            } else {
+                                count -= 2;
+                            }
+                        }
+                        for (int i = 0; i < 19; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for(int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+                    }
+                    if(count == 20) {
+
+                    } else{
+                        numLeft = random.nextInt(10);
+                        img_left.setImageResource(array.images1[numLeft]);
+                        img_left.startAnimation(a);
+                        text_left.setText(array.texts1[numLeft]);
+
+                        numRight = random.nextInt(10);
+
+
+                        while(numLeft == numRight) {
+                            numRight = random.nextInt(10);
+                        }
+
+                        img_right.setImageResource(array.images1[numRight]);
+                        img_right.startAnimation(a);
+                        text_right.setText(array.texts1[numRight]);
+
+                        img_right.setEnabled(true);
+                    }
+                }
+                return true;
+            }
+        });
+        //левая кнопка - конец
+
+        //правая кнопка - начало
+        img_right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    img_left.setEnabled(false);
+                    if(numLeft < numRight) {
+                        img_right.setImageResource(R.drawable.img_true);
+                    } else{
+                        img_right.setImageResource(R.drawable.img_false);
+                    }
+                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if(numLeft < numRight) {
+                        if(count < 20) {
+                            count++;
+                        }
+
+                        for (int i = 0; i < 20; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for(int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+
+                    } else {
+                        if(count > 0) {
+                            if(count == 1) {
+                                count = 0;
+                            } else {
+                                count -= 2;
+                            }
+                        }
+                        for (int i = 0; i < 19; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for(int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+                    }
+                    if(count == 20) {
+
+                    } else{
+                        numLeft = random.nextInt(10);
+                        img_left.setImageResource(array.images1[numLeft]);
+                        img_left.startAnimation(a);
+                        text_left.setText(array.texts1[numLeft]);
+
+                        numRight = random.nextInt(10);
+
+
+                        while(numLeft == numRight) {
+                            numRight = random.nextInt(10);
+                        }
+
+                        img_right.setImageResource(array.images1[numRight]);
+                        img_right.startAnimation(a);
+                        text_right.setText(array.texts1[numRight]);
+
+                        img_left.setEnabled(true);
+                    }
+                }
+                return true;
+            }
+        });
+        //правая кнопка - конец
 
     }
 
